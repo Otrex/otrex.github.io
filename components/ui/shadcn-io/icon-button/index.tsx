efbb90ee874c6a97b3582ab38269a3aaf312975c
Treasure O <treasure@tronweb.co>
2025-08-31 10:50:41 +0100

    fixes

--- components/ui/shadcn-io/icon-button/index.tsx
+++ components/ui/shadcn-io/icon-button/index.tsx
@@ -1,23 +1,24 @@
-'use client';
- 
-import * as React from 'react';
+"use client";
+
+import * as React from "react";
 import {
   motion,
   AnimatePresence,
   type HTMLMotionProps,
   type Transition,
-} from 'motion/react';
- 
-import { cn } from '@/lib/utils';
- 
+} from "motion/react";
+
+import { cn } from "@/lib/utils";
+import { Any } from "@/lib/type";
+
 const sizes = {
-  default: 'size-8 [&_svg]:size-5',
-  sm: 'size-6 [&_svg]:size-4',
-  md: 'size-10 [&_svg]:size-6',
-  lg: 'size-12 [&_svg]:size-7',
+  default: "size-8 [&_svg]:size-5",
+  sm: "size-6 [&_svg]:size-4",
+  md: "size-10 [&_svg]:size-6",
+  lg: "size-12 [&_svg]:size-7",
 };
- 
-type IconButtonProps = Omit<HTMLMotionProps<'button'>, 'color'> & {
+
+type IconButtonProps = Omit<HTMLMotionProps<"button">, "color"> & {
   icon: React.ElementType;
   active?: boolean;
   className?: string;
@@ -26,15 +27,15 @@ type IconButtonProps = Omit<HTMLMotionProps<'button'>, 'color'> & {
   color?: [number, number, number];
   transition?: Transition;
 };
- 
+
 function IconButton({
   icon: Icon,
   className,
   active = false,
   animate = true,
-  size = 'default',
+  size = "default",
   color = [59, 130, 246],
-  transition = { type: 'spring', stiffness: 300, damping: 15 },
+  transition = { type: "spring", stiffness: 300, damping: 15 },
   ...props
 }: IconButtonProps) {
   return (
@@ -43,13 +44,13 @@ function IconButton({
       className={cn(
         `group/icon-button cursor-pointer relative inline-flex size-10 shrink-0 rounded-full hover:bg-[var(--icon-button-color)]/10 active:bg-[var(--icon-button-color)]/20 text-[var(--icon-button-color)]`,
         sizes[size],
-        className,
+        className
       )}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
       style={
         {
-          '--icon-button-color': `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+          "--icon-button-color": `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
         } as React.CSSProperties
       }
       {...props}
@@ -58,11 +59,13 @@ function IconButton({
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 stroke-muted-foreground group-hover/icon-button:stroke-[var(--icon-button-color)]"
         aria-hidden="true"
       >
-        {React.createElement(Icon as React.ComponentType<any>, {
-          className: active ? 'fill-[var(--icon-button-color)]' : 'fill-transparent'
+        {React.createElement(Icon as React.ComponentType<Any>, {
+          className: active
+            ? "fill-[var(--icon-button-color)]"
+            : "fill-transparent",
         })}
       </motion.div>
- 
+
       <AnimatePresence mode="wait">
         {active && (
           <motion.div
@@ -73,11 +76,11 @@ function IconButton({
             exit={{ opacity: 0, scale: 0 }}
             transition={transition}
           >
-            {React.createElement(Icon as React.ComponentType<any>)}
+            {React.createElement(Icon as React.ComponentType<Any>)}
           </motion.div>
         )}
       </AnimatePresence>
- 
+
       <AnimatePresence>
         {animate && active && (
           <>
@@ -88,7 +91,7 @@ function IconButton({
               }}
               initial={{ scale: 1.2, opacity: 0 }}
               animate={{ scale: [1.2, 1.8, 1.2], opacity: [0, 0.3, 0] }}
-              transition={{ duration: 1.2, ease: 'easeInOut' }}
+              transition={{ duration: 1.2, ease: "easeInOut" }}
             />
             <motion.div
               className="absolute inset-0 z-10 rounded-full"
@@ -97,20 +100,20 @@ function IconButton({
               }}
               initial={{ scale: 1, opacity: 0 }}
               animate={{ scale: [1, 1.5], opacity: [0.8, 0] }}
-              transition={{ duration: 0.8, ease: 'easeOut' }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
             />
             {[...Array(6)].map((_, i) => (
               <motion.div
                 key={i}
                 className="absolute w-1 h-1 rounded-full bg-[var(--icon-button-color)]"
-                initial={{ x: '50%', y: '50%', scale: 0, opacity: 0 }}
+                initial={{ x: "50%", y: "50%", scale: 0, opacity: 0 }}
                 animate={{
                   x: `calc(50% + ${Math.cos((i * Math.PI) / 3) * 30}px)`,
                   y: `calc(50% + ${Math.sin((i * Math.PI) / 3) * 30}px)`,
                   scale: [0, 1, 0],
                   opacity: [0, 1, 0],
                 }}
-                transition={{ duration: 0.8, delay: i * 0.05, ease: 'easeOut' }}
+                transition={{ duration: 0.8, delay: i * 0.05, ease: "easeOut" }}
               />
             ))}
           </>
@@ -119,5 +122,5 @@ function IconButton({
     </motion.button>
   );
 }
- 
+
 export { IconButton, sizes, type IconButtonProps };
